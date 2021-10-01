688f070d577b6f73a92996c04a1afd2b8f72bdf3
Handsome boiz <90019917+Ngocminh12032002@users.noreply.github.com>
2021-10-01 09:08:13 +0700

    Update Sap dat day so.cpp

--- Sap dat day so.cpp
+++ Sap dat day so.cpp
@@ -1,29 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-long long mang[1000000], mang2[1000000];
-int main() {
-	int t;
-	cin >> t;
-	while (t--) {
-		long long n;
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			cin >> mang[i];
-		}
-		for (int i = 0; i < n; i++) {
-			mang2[i] = -1;
-		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (mang[i] == j) {
-					mang2[j] = j;
-				}
-			}
-		}
-		for (int i = 0; i < n; i++) {
-			cout << mang2[i] << " ";
-		}
-		cout << endl;
-	}
-	return 0;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        long long a[n],b[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            b[i]=-1;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(a[j]==i){
+                    b[i]=a[j];
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            cout<<b[i]<<" ";
+        }
+        cout<<endl;
+    }
 }
